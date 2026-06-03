096738aca129c04f6ab220917a524c2ab1e94bdc
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-06-03 23:19:40 +0700

    Add ApiDataSource entity

--- src/main/java/com/swp391/scientific_journal_tracker/entity/ApiDataSource.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/ApiDataSource.java
@@ -1,8 +1,25 @@
 package com.swp391.scientific_journal_tracker.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "ApiDataSources")
-
 public class ApiDataSource {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ApiDataSourceId", nullable = false, unique = true)
+    private Long apiDataSourceId;
+    @Column(name = "Name", nullable = false, columnDefinition = "VARCHAR(100) COLLATE utf8mb4_unicode_ci")
+    private String name;
+    @Column(name = "BaseURL", nullable = false, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
+    private String baseUrl;
+    @Column(name = "LastSyncTime", nullable = true)
+    private LocalDateTime lastSyncTime;
 }
