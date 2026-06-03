21d059c4ccde7b13e5a15efc6c74d9c14bf3e952
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-06-03 20:52:31 +0700

    Add keyword entity

--- src/main/java/com/swp391/scientific_journal_tracker/entity/Keyword.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/Keyword.java
@@ -1,5 +1,27 @@
 package com.swp391.scientific_journal_tracker.entity;
 
-public class Keyword {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Keywords", indexes = {
+        @Index(name = "idx_keyword_name", columnList = "Term")
+})
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Keyword {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "KeywordId", nullable = false, unique = true)
+    private Long keywordId;
+    @Column(name = "Term", nullable = false, unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
+    private String term;
 }
