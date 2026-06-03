6a7a577a0a3b6ede35a7bd7de1655ae722834cec
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-06-03 23:09:55 +0700

    Add ResearchTopic entity

--- src/main/java/com/swp391/scientific_journal_tracker/entity/ApiDataSource.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/ApiDataSource.java
@@ -1,5 +1,8 @@
 package com.swp391.scientific_journal_tracker.entity;
 
+@Entity
+@Table(name = "ApiDataSources")
+
 public class ApiDataSource {
 
 }
--- src/main/java/com/swp391/scientific_journal_tracker/entity/ResearchTopic.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/ResearchTopic.java
@@ -5,9 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ResearchTopics")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ResearchTopic {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
