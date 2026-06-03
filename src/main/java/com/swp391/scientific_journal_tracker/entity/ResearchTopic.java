8ade48b9e76914fc04493d57f5bef3159900e780
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-06-03 23:05:28 +0700

    Add ResearchTopic entity

--- src/main/java/com/swp391/scientific_journal_tracker/entity/ResearchTopic.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/ResearchTopic.java
@@ -1,5 +1,20 @@
 package com.swp391.scientific_journal_tracker.entity;
 
-public class ResearchTopic {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "ResearchTopics")
+public class ResearchTopic {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "ResearchTopicId", nullable = false, unique = true)
+    private Long researchTopicId;
+    @Column(name = "Name", nullable = false, columnDefinition = "VARCHAR(150) COLLATE utf8mb4_unicode_ci")
+    private String name;
+    @Column(name = "Description", columnDefinition = "TEXT COLLATE utf8mb4_unicode_ci")
+    private String description;
 }
