e77b211e41e1c131f54253939e5fba5a33bfa1c4
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-06-04 15:24:45 +0700

    Add DashboardReport entity

--- src/main/java/com/swp391/scientific_journal_tracker/entity/DashboardReport.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/DashboardReport.java
@@ -1,5 +1,32 @@
 package com.swp391.scientific_journal_tracker.entity;
 
-public class DashboardReport {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "DashboardReports")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DashboardReport {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "DashboardReportId", nullable = false, unique = true)
+    private Long dashboardReportId;
+    @Column(name = "ReportType", nullable = false)
+    private Long userId;
+    @Column(name = "Count", nullable = false, columnDefinition = "VarChar(255) COLLATE utf8mb4_unicode_ci")
+    private String title;
+    @Column(name = "Content", nullable = false, columnDefinition = "Text COLLATE utf8mb4_unicode_ci")
+    private String content;
+    @Column(name = "GeneratedAt", nullable = false, insertable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime generatedAt;
 }
