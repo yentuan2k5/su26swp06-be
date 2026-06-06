758fe4f970ae0775f49274245a767d2583ca6bb9
QuangTruong611 <truongtruong061105@gmail.com>
2026-06-07 00:26:55 +0700

    addBookmarkRepository

--- src/main/java/com/swp391/scientific_journal_tracker/repository/BookmarkRepository.java
+++ src/main/java/com/swp391/scientific_journal_tracker/repository/BookmarkRepository.java
@@ -9,16 +9,16 @@ import com.swp391.scientific_journal_tracker.entity.Bookmark;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-     // Lấy tất cả bookmark của 1 user
+    
     List<Bookmark> findByUserUserId(Long userId);
 
-    // Kiểm tra user đã bookmark bài báo chưa
+    
     boolean existsByUserUserIdAndResearchPaperResearchPaperId(
             Long userId,
             Long researchPaperId
     );
 
-    // Tìm bookmark cụ thể
+    
     Optional<Bookmark> findByUserUserIdAndResearchPaperResearchPaperId(
             Long userId,
             Long researchPaperId
