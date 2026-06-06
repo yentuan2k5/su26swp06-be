cc71f63dfe9421cd4f8e3fe432397d4286a5b4e2
QuangTruong611 <truongtruong061105@gmail.com>
2026-06-06 23:51:42 +0700

    addUserRepository

--- src/main/java/com/swp391/scientific_journal_tracker/entity/ResearchTopic.java
+++ src/main/java/com/swp391/scientific_journal_tracker/entity/ResearchTopic.java
@@ -3,8 +3,6 @@ package com.swp391.scientific_journal_tracker.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.ManyToAny;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/swp391/scientific_journal_tracker/repository/UserRepository.java
+++ src/main/java/com/swp391/scientific_journal_tracker/repository/UserRepository.java
@@ -1,5 +1,20 @@
 package com.swp391.scientific_journal_tracker.repository;
+import java.util.List;
+import java.util.Optional;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.swp391.scientific_journal_tracker.entity.User;
+import com.swp391.scientific_journal_tracker.entity.User.Role;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<User> findByRole(Role role);
+
+    List<User> findByNameContainingIgnoreCase(String keyword);
 }
