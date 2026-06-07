f50031f827c653f16cbffc0a6902f98f7704f7c2
QuangTruong611 <truongtruong061105@gmail.com>
2026-06-07 21:46:20 +0700

    addKeywordRepository

--- src/main/java/com/swp391/scientific_journal_tracker/repository/KeywordRepository.java
+++ src/main/java/com/swp391/scientific_journal_tracker/repository/KeywordRepository.java
@@ -1,5 +1,17 @@
 package com.swp391.scientific_journal_tracker.repository;
+import java.util.List;
+import java.util.Optional;
 
-public class KeywordRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.swp391.scientific_journal_tracker.entity.Keyword;
+
+@Repository
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    Optional<Keyword> findbyterm(String term);
+
+    List<Keyword> findByTermContainingIgnoreCase(String term);
+
+    boolean existsByTerm(String term);
 }
