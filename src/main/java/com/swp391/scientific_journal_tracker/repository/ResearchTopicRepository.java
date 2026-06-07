8e306215254eff3d48cbea95b80b7fc9c8693fc5
QuangTruong611 <truongtruong061105@gmail.com>
2026-06-07 23:32:54 +0700

    addResearchTopicRepository

--- src/main/java/com/swp391/scientific_journal_tracker/repository/ResearchTopicRepository.java
+++ src/main/java/com/swp391/scientific_journal_tracker/repository/ResearchTopicRepository.java
@@ -1,5 +1,23 @@
 package com.swp391.scientific_journal_tracker.repository;
+import java.util.List;
+import java.util.Optional;
 
-public class ResearchTopicRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.swp391.scientific_journal_tracker.entity.ResearchTopic;
+@Repository
+
+public interface ResearchTopicRepository extends JpaRepository<ResearchTopic, Long> {
+    Optional<ResearchTopic> findByName(String name); 
+
+    boolean existsByName(String name);
+
+    List<ResearchTopic> findByNameContainingIgnoreCase(String name);
+
+    List<ResearchTopic> findByDescriptionContainingIgnoreCase(String description);
+
+    
+
+    
 }
